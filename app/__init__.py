4fa461369b9b1547564d2d4375846c5ff043e3e2
Krysia Banaszewska <krysia.banaszewskaa@gmail.com>
2023-05-16 16:00:55 +0200

    upload files

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = '783fbaf9c60eaaa9a455b1f839699d0af9d6a87caeab71efa13b71cb85a61ca4'
 
